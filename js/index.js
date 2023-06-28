ddc1972938e29b7abf4333de366d42383d75cf03
MaegiW <maggie90228@gmail.com>
2023-06-28 22:54:40 +0800

    628

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("loginbutton").addEventListener("click", function(){
+    document.querySelector(".poppup").style.display = "flex";
+})
+document.querySelector(".close-btn").addEventListener("click", function(){
+    document.querySelector(".poppup").style.display = "none";
+})
+
