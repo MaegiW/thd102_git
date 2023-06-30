ed6bb4e369f40d01dd156d61f5a86264b534bd51
MaegiW <maggie90228@gmail.com>
2023-06-30 22:56:02 +0800

    630

---
+++ js/products.js
@@ -0,0 +1,8 @@
+//login
+document.getElementById("loginbutton").addEventListener("click", function(){
+    document.querySelector(".poppup").style.display = "flex";
+ })
+ document.querySelector(".close-btn").addEventListener("click", function(){
+    document.querySelector(".poppup").style.display = "none";
+ })
+ 
