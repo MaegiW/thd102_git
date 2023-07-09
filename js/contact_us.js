6ca6948a2ede9623001f0e85747dabd9e6008668
MaegiW <maggie90228@gmail.com>
2023-07-09 14:37:50 +0800

    709

--- js/contact_us.js
+++ js/contact_us.js
@@ -1,7 +1,23 @@
-//login
-document.getElementById("loginbutton").addEventListener("click", function(){
+//login1
+document.getElementById("loginbutton1").addEventListener("click", function(){
+    document.querySelector(".poppup").style.display = "flex";
+})
+//login2
+document.getElementById("loginbutton2").addEventListener("click", function(){
     document.querySelector(".poppup").style.display = "flex";
 })
 document.querySelector(".close-btn").addEventListener("click", function(){
     document.querySelector(".poppup").style.display = "none";
-});
+})
+
+
+// RWD/////
+
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
+
