905a0a7fef2cdc53c861e9a06995a7b63d2aec2a
DESKTOP-S486R7A\i5-14500 <maggie90228@gmail.com>
2025-06-09 14:05:54 +0800

    20250609

--- js/contact_us.js
+++ js/contact_us.js
@@ -1,23 +1,58 @@
 //login1
-document.getElementById("loginbutton1").addEventListener("click", function(){
-    document.querySelector(".poppup").style.display = "flex";
-})
+document.getElementById("loginbutton1").addEventListener("click", function () {
+  document.querySelector(".poppup").style.display = "flex";
+});
 //login2
-document.getElementById("loginbutton2").addEventListener("click", function(){
-    document.querySelector(".poppup").style.display = "flex";
-})
-document.querySelector(".close-btn").addEventListener("click", function(){
-    document.querySelector(".poppup").style.display = "none";
-})
-
+document.getElementById("loginbutton2").addEventListener("click", function () {
+  document.querySelector(".poppup").style.display = "flex";
+});
+document.querySelector(".close-btn").addEventListener("click", function () {
+  document.querySelector(".poppup").style.display = "none";
+});
 
 // RWD/////
 
 let menu = document.querySelector("#menu-icon");
-let navbar = document.querySelector('.navbar');
+let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
-}
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("open");
+};
 
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form"),
+  contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  // serviceID - templateID - #form - publicKey
+  emailjs
+    .sendForm(
+      "service_ztaq4tq",
+      "template_o6sodp6",
+      "#contact-form",
+      "oKMdzUtRX5sU-MxRU"
+    )
+    .then(
+      () => {
+        // Show sent message
+        contactMessage.textContent = "Message sent successfully ✅";
+
+        // Remove message after five seconds
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 5000);
+
+        // Clear input fields
+        contactForm.reset();
+      },
+      () => {
+        // Show error message
+        contactMessage.textContent = "Something went wrong ❌";
+      }
+    );
+};
+
+contactForm.addEventListener("submit", sendEmail);
